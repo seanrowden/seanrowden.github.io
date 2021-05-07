92e3f97956906eda173d7892deff460eb976fc6e
seanrowden <enusdench@gmail.com>
2021-05-07 13:59:30 -0400

    Add files via upload
    transfered all files from test-site folder on computer, so they're hosted on Github

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+  let mySrc = myImage.getAttribute('src');
+  if(mySrc == 'images/sadball.jpg') {
+    myImage.setAttribute('src','images/madball.jpg');
+  } else {
+    myImage.setAttribute('src','images/sadball.jpg');
+  }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+function setUserName() {
+  let myName = prompt('Please enter your name.');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
