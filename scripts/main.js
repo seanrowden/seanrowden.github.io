796b8014572dd2effe3d60e174312e2b66eef37a
seanrowden <enusdench@gmail.com>
2021-05-09 08:29:42 -0400

    Update main.js
    Changed heading text from default to customize as appropriate for specific site.

--- scripts/main.js
+++ scripts/main.js
@@ -22,7 +22,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.textContent = 'Mozilla is cool, ' + myName;
+    myHeading.textContent = 'Balls are cool, ' + myName;
   }
 }
 
